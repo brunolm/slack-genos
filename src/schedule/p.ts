f6cffd4d65603d09b680c749aeecc1a9204a6dd8
Bruno Leonardo Michels <brunolm7@gmail.com>
2017-10-08 19:57:45 -0300

    Notification 2 minutes before for btd

--- src/schedule/p.ts
+++ src/schedule/p.ts
@@ -1,5 +1,6 @@
 import * as memory from '../services/memory';
-import * as moment from 'moment';
+import * as momentRaw from 'moment';
+import * as momentTz from 'moment-timezone';
 import * as path from 'path';
 
 import AhgoraService from 'btd-ahgora/built/ahgora-service';
@@ -8,6 +9,8 @@ import logger from '../logger';
 
 const log = logger('btd:notifications');
 
+const timers = {};
+
 async function runTasks() {
   const mem = await memory.load();
   const files = await findFiles('usersettings/*.json');
@@ -15,6 +18,8 @@ async function runTasks() {
   for (const file of files) {
     try {
       const settings = require(path.join(__dirname, '../../', file));
+
+      const moment = (...args) => momentTz(...args).tz(settings.tz || 'America/Sao_Paulo');
 
       const service = new AhgoraService(settings.btd);
 
@@ -26,33 +31,94 @@ async function runTasks() {
 
       const dataTime = await service.getTimes();
       const today = service.getToday(dataTime.times);
+
+      /** DEBUG *
+      dataTime.times[moment().format('DD/MM/YYYY')].beats = [
+        '07:00',
+        '11:30',
+        '12:30',
+        // '17:00',
+      ];
+      dataTime.times[moment().format('DD/MM/YYYY')].beatsRaw = dataTime.times[moment().format('DD/MM/YYYY')].beats.join(', ');
+      /**/
+
       const result = service.parseResult(dataTime.times);
 
       if (today.beats.length <= 1) {
         continue;
       }
 
-      const notificationFromMem = deepRead(mem, ['btd', 'notifications', settings.id, today.beats.length]);
+      const deepKey = ['btd', 'notifications', settings.id, today.beats.length];
 
+      // check if notification was already sent
+      let hasFirst = false;
+      let hasSecond = false;
+
+      const notificationFromMem = deepRead(mem, deepKey);
       if (notificationFromMem) {
         const isFromToday = moment(notificationFromMem.date) >= moment().startOf('day')
           && moment(notificationFromMem.date) <= moment().endOf('day');
 
         if (isFromToday) {
-          continue;
+          hasFirst = notificationFromMem.first;
+          hasSecond = notificationFromMem.second;
+
+          if (hasFirst && hasSecond) {
+            continue;
+          }
         }
       }
 
-      deepAssign(mem, ['btd', 'notifications', settings.id, today.beats.length], {
-        date: moment(),
-      });
+      // if there isn't memory or timer then start one
+      if (!notificationFromMem || !hasSecond) {
+        const timer = deepRead(timers, deepKey);
 
-      memory.set({ ...mem });
+        if (!timer) {
+          const secondNotifyAt = moment(result.match(/(\d{2}:\d{2})/)[1], 'HH:mm').add(-2, 'minutes');
 
-      process.send({
-        channel: settings.id,
-        text: result,
-      });
+          deepAssign(timers, deepKey, setInterval(() => {
+            const memOfNotification = deepRead(mem, deepKey);
+
+            if (memOfNotification.second) {
+              clearInterval(timer);
+              return;
+            }
+
+            if (moment() >= secondNotifyAt) {
+              deepAssign(mem, deepKey, {
+                first: true,
+                second: true,
+                secondNotifyAt,
+                date: moment(),
+              });
+              memory.set({ ...mem });
+
+              process.send({
+                channel: settings.id,
+                text: `:warning: <@${settings.id}> --> ${result}`,
+              });
+
+              clearInterval(timer);
+            }
+          }, 5000));
+
+          deepAssign(mem, deepKey, {
+            first: true,
+            second: false,
+            secondNotifyAt,
+            date: moment(),
+          });
+
+          memory.set({ ...mem });
+        }
+      }
+
+      if (!hasFirst) {
+        process.send({
+          channel: settings.id,
+          text: result,
+        });
+      }
     }
     catch (err) {
       log.debug('Error making request', err, 'for', file);
@@ -86,5 +152,5 @@ function deepRead(obj, keyPath) {
 
 export default function start() {
   runTasks();
-  setInterval(runTasks, moment.duration(15, 'minutes').asMilliseconds());
+  setInterval(runTasks, momentRaw.duration(12, 'minutes').asMilliseconds());
 }
